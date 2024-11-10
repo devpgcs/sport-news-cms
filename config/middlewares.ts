a26ce137ab3dfc26993fb06d2dbf5e10ffb46be0
Pedro Castro <devpgcs@hotmail.com>
2024-11-09 19:00:53 -0500

    First commit :rocket:

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,28 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "script-src": ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "cdn.jsdelivr.net",
+            "strapi.io",
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,44 @@
-export default () => ({});
+export default ({ env }) => {
+  console.log(
+    {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        ACL: env("AWS_ACL", "public-read"), // 'private' if you want to make the uploaded files private
+        Bucket: env("AWS_BUCKET"),
+      },
+    },
+    "NEW?"
+  );
+
+  return {
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          s3Options: {
+            credentials: {
+              accessKeyId: env("AWS_ACCESS_KEY_ID"),
+              secretAccessKey: env("AWS_ACCESS_SECRET"),
+            },
+            region: env("AWS_REGION"),
+            params: {
+              //   ACL: env("AWS_ACL", "public-read"),
+              //   signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+              Bucket: env("AWS_BUCKET"),
+            },
+          },
+        },
+        actionOptions: {
+          upload: {
+            ACL: null,
+          },
+          uploadStream: {
+            ACL: null,
+          },
+        },
+      },
+    },
+  };
+};
